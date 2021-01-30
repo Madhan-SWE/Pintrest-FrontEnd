667cb4975847dd32d39a95da1083b6d4d9b2591d
madhan-swe <madhankumarcvkl@gmail.com>
2021-01-30 14:00:10 +0530

    Initial commit - File upload test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FileUpload from './fileupload'
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
